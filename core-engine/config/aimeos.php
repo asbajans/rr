103115f15ccf9006440bc7bc56b6193a48c27f23
asbajans <162858677+asbajans@users.noreply.github.com>
2026-07-06 21:47:35 +0300

    fix: aimeos resource db config (host mysql TCP), resource.php-dist kopyalama

--- core-engine/config/aimeos.php
+++ core-engine/config/aimeos.php
@@ -12,6 +12,14 @@ return [
         'disableSites' => false,
     ],
     'resource' => [
+        'db' => [
+            'adapter' => 'mysql',
+            'host' => env('DB_HOST', 'mysql'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'rahatio'),
+            'username' => env('DB_USERNAME', 'rahatio'),
+            'password' => env('DB_PASSWORD', 'rahatio'),
+        ],
         'fs' => [
             'adapter' => 'standard',
             'basedir' => 'uploads',
