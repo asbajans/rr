cd11df111059e4c0a57c54a73e0e47931e104db3
asbajans <162858677+asbajans@users.noreply.github.com>
2026-07-06 21:32:32 +0300

    fix: Aimeos filesystem resource config eklendi

--- core-engine/config/aimeos.php
+++ core-engine/config/aimeos.php
@@ -11,4 +11,40 @@ return [
     'shop' => [
         'disableSites' => false,
     ],
+    'resource' => [
+        'fs' => [
+            'adapter' => 'standard',
+            'basedir' => 'uploads',
+            'baseurl' => '/file',
+        ],
+        'fs-admin' => [
+            'adapter' => 'standard',
+            'basedir' => 'uploads/admin',
+            'baseurl' => '/file',
+        ],
+        'fs-export' => [
+            'adapter' => 'standard',
+            'basedir' => 'tmp/export',
+            'baseurl' => '/file',
+        ],
+        'fs-import' => [
+            'adapter' => 'standard',
+            'basedir' => 'tmp/import',
+            'baseurl' => '/file',
+        ],
+        'fs-secure' => [
+            'adapter' => 'standard',
+            'basedir' => 'uploads/secure',
+            'baseurl' => '/file',
+        ],
+        'fs-media' => [
+            'adapter' => 'standard',
+            'basedir' => 'uploads/media',
+            'baseurl' => '/file',
+        ],
+        'mq' => [
+            'adapter' => 'Standard',
+            'basedir' => 'tmp/mq',
+        ],
+    ],
 ];
