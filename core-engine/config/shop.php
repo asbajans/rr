74df3f04015269545e74b421d066942a6e2a6ca1
asbajans <162858677+asbajans@users.noreply.github.com>
2026-07-06 21:55:08 +0300

    fix: shop.php resource wrapper ekle (resource/db/host override)

--- core-engine/config/shop.php
+++ core-engine/config/shop.php
@@ -1,11 +1,13 @@
 <?php
 
 return [
-    'db' => [
-        'host' => env('DB_HOST', 'mysql'),
-        'port' => env('DB_PORT', '3306'),
-        'database' => env('DB_DATABASE', 'rahatio'),
-        'username' => env('DB_USERNAME', 'rahatio'),
-        'password' => env('DB_PASSWORD', 'rahatio'),
+    'resource' => [
+        'db' => [
+            'host' => env('DB_HOST', 'mysql'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'rahatio'),
+            'username' => env('DB_USERNAME', 'rahatio'),
+            'password' => env('DB_PASSWORD', 'rahatio'),
+        ],
     ],
 ];
