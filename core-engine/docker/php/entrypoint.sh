6fe40d307726bcf8992df57df5727a346706cd0e
asbajans <162858677+asbajans@users.noreply.github.com>
2026-07-06 15:35:40 +0300

    feat: APP_KEY otomatik oluşturma (entrypoint.sh)

---
+++ core-engine/docker/php/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -e
+
+if [ -z "$APP_KEY" ] || [ "$APP_KEY" = " " ]; then
+    echo "Generating APP_KEY..."
+    php artisan key:generate --force
+fi
+
+exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
