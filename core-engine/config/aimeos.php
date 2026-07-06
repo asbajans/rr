f882dbecd9270d382c31c02a5c146fef4f64b26a
asbajans <162858677+asbajans@users.noreply.github.com>
2026-07-06 21:50:56 +0300

    fix: aimeos config key yapisini resource.php ile esitle (db/fs root seviyede)

--- core-engine/config/aimeos.php
+++ core-engine/config/aimeos.php
@@ -11,48 +11,45 @@ return [
     'shop' => [
         'disableSites' => false,
     ],
-    'resource' => [
-        'db' => [
-            'adapter' => 'mysql',
-            'host' => env('DB_HOST', 'mysql'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'rahatio'),
-            'username' => env('DB_USERNAME', 'rahatio'),
-            'password' => env('DB_PASSWORD', 'rahatio'),
-        ],
-        'fs' => [
-            'adapter' => 'standard',
-            'basedir' => 'uploads',
-            'baseurl' => '/file',
-        ],
-        'fs-admin' => [
-            'adapter' => 'standard',
-            'basedir' => 'uploads/admin',
-            'baseurl' => '/file',
-        ],
-        'fs-export' => [
-            'adapter' => 'standard',
-            'basedir' => 'tmp/export',
-            'baseurl' => '/file',
-        ],
-        'fs-import' => [
-            'adapter' => 'standard',
-            'basedir' => 'tmp/import',
-            'baseurl' => '/file',
-        ],
-        'fs-secure' => [
-            'adapter' => 'standard',
-            'basedir' => 'uploads/secure',
-            'baseurl' => '/file',
-        ],
-        'fs-media' => [
-            'adapter' => 'standard',
-            'basedir' => 'uploads/media',
-            'baseurl' => '/file',
-        ],
-        'mq' => [
-            'adapter' => 'Standard',
-            'basedir' => 'tmp/mq',
-        ],
+    'db' => [
+        'host' => env('DB_HOST', 'mysql'),
+        'port' => env('DB_PORT', '3306'),
+        'database' => env('DB_DATABASE', 'rahatio'),
+        'username' => env('DB_USERNAME', 'rahatio'),
+        'password' => env('DB_PASSWORD', 'rahatio'),
+    ],
+    'fs' => [
+        'adapter' => 'standard',
+        'basedir' => 'uploads',
+        'baseurl' => '/file',
+    ],
+    'fs-admin' => [
+        'adapter' => 'standard',
+        'basedir' => 'uploads/admin',
+        'baseurl' => '/file',
+    ],
+    'fs-export' => [
+        'adapter' => 'standard',
+        'basedir' => 'tmp/export',
+        'baseurl' => '/file',
+    ],
+    'fs-import' => [
+        'adapter' => 'standard',
+        'basedir' => 'tmp/import',
+        'baseurl' => '/file',
+    ],
+    'fs-secure' => [
+        'adapter' => 'standard',
+        'basedir' => 'uploads/secure',
+        'baseurl' => '/file',
+    ],
+    'fs-media' => [
+        'adapter' => 'standard',
+        'basedir' => 'uploads/media',
+        'baseurl' => '/file',
+    ],
+    'mq' => [
+        'adapter' => 'Standard',
+        'basedir' => 'tmp/mq',
     ],
 ];
