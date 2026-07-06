59408167903e4c6c0e39ee043ed9c4c1713607dd
asbajans <162858677+asbajans@users.noreply.github.com>
2026-07-06 22:02:59 +0300

    fix: resource override'a email/fs/mq key'leri ekle (subtree replace)

--- core-engine/config/shop.php
+++ core-engine/config/shop.php
@@ -12,5 +12,17 @@ return [
             'stmt' => ["SET SESSION sort_buffer_size=2097144; SET NAMES 'utf8'; SET SESSION sql_mode='ANSI'"],
             'opt-persistent' => false,
         ],
+        'email' => [
+            'from-email' => env('MAIL_FROM_ADDRESS', 'me@localhost'),
+            'from-name' => env('MAIL_FROM_NAME', 'Rahatio'),
+        ],
+        'fs' => [
+            'adapter' => 'Standard',
+            'basedir' => '.',
+        ],
+        'mq' => [
+            'adapter' => 'Standard',
+            'db' => 'db',
+        ],
     ],
 ];
