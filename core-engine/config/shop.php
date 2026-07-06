4d149ecb0475859f43b1871686c3cde07c86c967
asbajans <162858677+asbajans@users.noreply.github.com>
2026-07-06 21:53:36 +0300

    fix: aimeos db config i shop.php'e taşı (config('shop') ile okunur)

---
+++ core-engine/config/shop.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'db' => [
+        'host' => env('DB_HOST', 'mysql'),
+        'port' => env('DB_PORT', '3306'),
+        'database' => env('DB_DATABASE', 'rahatio'),
+        'username' => env('DB_USERNAME', 'rahatio'),
+        'password' => env('DB_PASSWORD', 'rahatio'),
+    ],
+];
