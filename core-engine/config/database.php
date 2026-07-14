33b7dfd954a806770b0e16f94f5dbe4e1096085c
asbajans <162858677+asbajans@users.noreply.github.com>
2026-07-14 07:21:12 +0300

    fix(core): add redis 'default' connection config (queue worker was crashing)

--- core-engine/config/database.php
+++ core-engine/config/database.php
@@ -17,4 +17,26 @@ return [
             'strict' => true,
         ],
     ],
+
+    'redis' => [
+        'client' => env('REDIS_CLIENT', 'predis'),
+        'options' => [
+            'cluster' => env('REDIS_CLUSTER', 'redis'),
+            'prefix' => env('REDIS_PREFIX', 'rahatio_database_'),
+        ],
+        'default' => [
+            'url' => env('REDIS_URL'),
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'password' => env('REDIS_PASSWORD'),
+            'port' => env('REDIS_PORT', '6379'),
+            'database' => env('REDIS_DB', '0'),
+        ],
+        'cache' => [
+            'url' => env('REDIS_URL'),
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'password' => env('REDIS_PASSWORD'),
+            'port' => env('REDIS_PORT', '6379'),
+            'database' => env('REDIS_CACHE_DB', '1'),
+        ],
+    ],
 ];
